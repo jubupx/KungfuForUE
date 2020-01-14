ad41c3ca11825294c404ab81a1d0936c82500985
jubupx <329052613@qq.com>
2020-01-15 07:56:45 +0800

    1 . 增加子模块

---
+++ Source/KungfuForUE/KFSpriteComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/MeshComponent.h"
+#include "KFSpriteComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KUNGFUFORUE_API UKFSpriteComponent : public UMeshComponent
+{
+	GENERATED_BODY()
+	
+};
